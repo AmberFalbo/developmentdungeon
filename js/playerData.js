21cbb8918c040eaba148dc6434afe7c9873c50c7
Trevor Stubbs <stubbste@gmail.com>
2020-05-18 14:58:38 -0700

    playerData framework

---
+++ js/playerData.js
@@ -0,0 +1,36 @@
+'use strict';
+var allPlayers = [];
+
+var currentPlayer = allPlayers[allPlayers.length-1];
+
+function PlayerData(name, memoryPlayed=false, keywordPlayed=false){
+  this.name = name;
+  this.score = 0;
+  this.memoryPlayed = memoryPlayed;
+  this.keywordPlayed = keywordPlayed;
+  allPlayers.push(this);
+}
+
+// set Local storage with player data
+function setLocalStorage(){
+  var playerSetter = JSON.stringify(allPlayers);
+  localStorage.setItem('players', playerSetter);
+}
+
+// get local storage player data
+function getLocalStorage(){
+  console.log('Full: ' + allPlayers);
+  allPlayers = [];
+  console.log('Empty: ' + allPlayers);
+  var playerGetter = localStorage.getItem('players');
+  var parsedPlayers = JSON.parse(playerGetter);
+  for(var i = 0; i<parsedPlayers.length;i++){
+    new PlayerData(
+      parsedPlayers[i].name,
+      parsedPlayers[i].score,
+      parsedPlayers[i].memoryPlayed,
+      parsedPlayers[i].keywordPlayed
+    );
+  }
+  console.log('Full?: ' + allPlayers);
+}
