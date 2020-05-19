83e249bab0caa224d7ab85227dfbdf225b883274
Trevor Stubbs <stubbste@gmail.com>
2020-05-18 18:59:11 -0700

    user data entry

--- js/playerName.js
+++ js/playerName.js
@@ -2,4 +2,11 @@
 
 document.getElementById('playerName').addEventListener('submit', function handler(event){
   event.preventDefault();
+
+  var nameOfPlayer = event.target.namePlayer.value;
+  new PlayerData(nameOfPlayer);
+
+  setLocalStorage();
+
+  location.href = 'memory.html';
 });
