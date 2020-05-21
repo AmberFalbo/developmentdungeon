e10cec5e118dcc3e167e4cb27a823d43f81355c8
Amber Falbo <amberannfalbo@gmail.com>
2020-05-21 12:00:03 -0700

    adjustments to the memory game

--- js/memoryGame/memoryLogic.js
+++ js/memoryGame/memoryLogic.js
@@ -35,7 +35,6 @@ var backgroundColor = ['blue', 'red', 'green', 'gray', 'yellow', 'pink', 'orange
 document.getElementById('startGame').addEventListener('submit', function handler(event){
   event.preventDefault();
   console.log('Is this working?');
-  loadRenderTable();
   document.getElementById('startGame').style.visibility = 'hidden';
 });
 
